9447f991a8a3b4e7e0aa06c37d1695301d91c019
noura <nouraabdullahahq@gmail.com>
2023-12-19 09:32:23 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,19 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Noura Alqahtani on 17/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+//    @StateObject private var themeSettings = ThemeSettings()
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+//                .environmentObject(themeSettings)
+        }
+    }
+}
